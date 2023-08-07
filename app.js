8506cdfaaa4a82c0bfc2b60022fe158ba3fc02f1
username-matthew <matthew@olk1.com>
2023-08-07 14:39:31 +0100

    add 30 words copy + next link + tweak styling

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const images = document.querySelectorAll('.anim');
 const images2 = document.querySelectorAll('.anim2');
+const images3 = document.querySelectorAll('.anim3');
 
 const observer1 = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
@@ -21,12 +22,26 @@ const observer2 = new IntersectionObserver((entries) => {
   });
 });
 
+const observer3 = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.intersectionRatio > 0) {
+      entry.target.style.animation = `anim3 1s ${entry.target.dataset.delay} forwards`;
+    } else {
+      entry.target.style.animation = 'none';
+    }
+  });
+});
+
 images.forEach(image => {
   observer1.observe(image);
 });
 
 images2.forEach(image => {
   observer2.observe(image);
+});
+
+images3.forEach(image => {
+  observer3.observe(image);
 });
 
 // Reset animations after 60 seconds
@@ -42,5 +57,11 @@ setInterval(() => {
     image.style.animation = 'none';
     setTimeout(() => observer2.observe(image), 0);
   });
-}, 60000); // 60 seconds in milliseconds
+  
+  // images3.forEach(image => {
+  //   observer3.unobserve(image);
+  //   image.style.animation = 'none';
+  //   setTimeout(() => observer3.observe(image), 0);
+  // });
+}, 180000); // milliseconds
   
